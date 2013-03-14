095e0599f5f04338904d49577a2448d069c59992
b66549 <rvelasco@fullsail.edu>
2013-03-13 18:37:19 -0700

    Added getElementById calls for all data.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 // Name: Rolando Velasco
 // Term: VFW 1303
 // Project Part 2
+
+var myGoalHeadline = document.getElementById("goalheadline");
+
+var myGoalType = document.getElementById("goaltype");
+
+var myDeadline = document.getElementById("deadline");
+
+var mySize = document.getElementById("size");
+
+var myDetails = document.getElementById("details");
+
+var myGoalComplete = document.getElementById("goalcomplete");
