cee807f354c7c85c826b276389868c688af7269d
Rolando Velasco <rvelasco@fullsail.edu>
2013-03-14 08:19:24 -0700

    modified additem.html, started code for main.js.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,33 @@
 // Term: VFW 1303
 // Project Part 2
 
-var myGoalHeadline = document.getElementById("goalheadline");
+var addNewGoal = function() {
 
-var myGoalType = document.getElementById("goaltype");
+	// Initialize the local variables with the html elements
+	var myGoalHeadline = document.getElementById("goalheadline");
+	var myGoalType = document.getElementById("goaltype");
+	var myDeadline = document.getElementById("deadline");
+	var mySize = document.getElementById("size");
+	var myDetails = document.getElementById("details");
+	var myGoalComplete = document.getElementById("goalcomplete");
 
-var myDeadline = document.getElementById("deadline");
+	// Test output
+	console.log(myGoalHeadline + "\n" + myGoalType.value + "\n" + myDeadline.value + "\n" + mySize.value + "\n" + myDetails.value + "\n" + myGoalComplete.checked);
 
-var mySize = document.getElementById("size");
+	for
+	return;
+}
 
-var myDetails = document.getElementById("details");
+var displayGoalsList = function() {
+	for (var i = 0, j = localStorage.length; i < j; i++) {
+		currentKey = localStorage.key[i];
+		currentValue = localStorage.getItem(currentKey);
+	}
+	var myList = document.getElement("ul");
+}
 
-var myGoalComplete = document.getElementById("goalcomplete");
+
+var saveGoalButton = document.getElementById("savegoal");
+saveGoalButton.addEventListener("click", addNewGoal);
+
+// var displayData = document.getElementById(
