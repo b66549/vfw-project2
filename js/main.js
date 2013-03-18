1fccc793ed590756bcfc9d6d9b37be250e6384b0
Rolando Velasco <rvelasco@fullsail.edu>
2013-03-18 08:08:49 -0700

    modified tags in additem.html, code for saving data and displaying data in main.js

--- js/main.js
+++ js/main.js
@@ -2,33 +2,51 @@
 // Term: VFW 1303
 // Project Part 2
 
-var addNewGoal = function() {
-
-	// Initialize the local variables with the html elements
-	var myGoalHeadline = document.getElementById("goalheadline");
-	var myGoalType = document.getElementById("goaltype");
-	var myDeadline = document.getElementById("deadline");
-	var mySize = document.getElementById("size");
-	var myDetails = document.getElementById("details");
-	var myGoalComplete = document.getElementById("goalcomplete");
-
-	// Test output
-	console.log(myGoalHeadline + "\n" + myGoalType.value + "\n" + myDeadline.value + "\n" + mySize.value + "\n" + myDetails.value + "\n" + myGoalComplete.checked);
-
-	for
-	return;
-}
-
-var displayGoalsList = function() {
-	for (var i = 0, j = localStorage.length; i < j; i++) {
-		currentKey = localStorage.key[i];
-		currentValue = localStorage.getItem(currentKey);
+// Line to wait until the DOM is ready
+window.addEventListener("DOMContentLoaded", function() {
+	var addNewGoal = function() {
+	
+		// Initialize the local variables with the html elements
+		var myGoalHeadline = document.getElementById("goalheadline");
+		var myGoalType = document.getElementById("goaltype");
+		var myDeadline = document.getElementById("deadline");
+		var mySize = document.getElementById("size");
+		var myDetails = document.getElementById("details");
+		var myGoalComplete = document.getElementById("goalcomplete");
+	
+		// Initialize the array for the form data and then check if our checkbox is checked or not, then enter data
+		var myArray = [];
+		isChecked = myGoalComplete.checked;	
+		if (isChecked) {
+			myArray = [myGoalHeadline.value, myGoalType.value, myDeadline.value, mySize.value, myDetails.value, myGoalComplete.value];
+		} else {
+			myArray = [myGoalHeadline.value, myGoalType.value, myDeadline.value, mySize.value, myDetails.value];
+		};
+		
+		// Test output
+		console.log(myArray);
+		
+			
+		return;
 	}
-	var myList = document.getElement("ul");
-}
+	
+	var displayGoalsList = function() {
+	
+		var myDisplayDataDiv = document.getElementById("displaydata");
+		
+		for (var i = 0, j = localStorage.length; i < j; i++) {
+			currentKey = localStorage.key[i];
+			currentValue = localStorage.getItem(currentKey);
+			var myList = document.createElement("p");
+			myList.innerHTML = currentValue;
+			myDisplayDataDiv.appendChild(myList);
+		}
+	}
+	
+	
+	var saveGoalButton = document.getElementById("savegoal");
+	saveGoalButton.addEventListener("click", addNewGoal);
+	
+	// var displayData = document.getElementById(
 
-
-var saveGoalButton = document.getElementById("savegoal");
-saveGoalButton.addEventListener("click", addNewGoal);
-
-// var displayData = document.getElementById(
+});
