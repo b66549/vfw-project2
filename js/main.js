7c141603afe0836fbbb9301bc3689c2bb8a4de34
Rolando Velasco <rvelasco@fullsail.edu>
2013-03-21 12:33:50 -0700

    Finished files. Modified additem.html, finished code for display data and clear local storage in main.js.

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ window.addEventListener("DOMContentLoaded", function() {
 	}
 	
 	function getGoalCompleteCheckBoxValue() {
-		if(document.getElementById(goalcomplete).checked) {
-			goalCompleteValue = document.getElementById(goalcomplete).value;
+		if(document.getElementById("goalcomplete").checked) {
+			goalCompleteValue = document.getElementById("goalcomplete").value;
 		} else {
 			goalCompleteValue = "Goal Not Yet Achieved";
 		}
@@ -34,15 +34,18 @@ window.addEventListener("DOMContentLoaded", function() {
 		// random number generator
 		var uniqueID = Math.floor(Math.random() * 1000001);
 		
+		// Find out if Goal Complete checkbox is checked or not
+		getGoalCompleteCheckBoxValue();
+		
 		// Initialize the local variables with the html elements
 		// Create an Object with each property an array that contains the form label and the input value
 		var goalObj 			= {};
 		goalObj.goalHeadline 	= ["Goal:", document.getElementById("goalheadline").value];
-		goalObj.goalType 		= ["Goal Type:", document.getElementById("goaltype").value];
+		goalObj.goalType 		= ["Goal Type:", document.getElementById("types").value];
 		goalObj.deadline 		= ["Deadline:", document.getElementById("deadline").value];
 		goalObj.size 			= ["Size:", document.getElementById("size").value];
 		goalObj.details 		= ["Details", document.getElementById("details").value];
-		goalObj.goalcomplete 	= ["Goal Complete:", goalCompleteValue]; document.getElementById("goalcomplete").checked];
+		goalObj.goalcomplete 	= ["Goal Complete:", goalCompleteValue];
 			
 		// Save the data into local Storage
 		// Use Stringify to convert the object into a string
@@ -50,31 +53,84 @@ window.addEventListener("DOMContentLoaded", function() {
 		alert("Goal Saved!");	
 		return;
 	}
-/*	
-	var displayGoalsList = function() {
 	
-		var myDisplayDataDiv = document.getElementById("displaydata");
+	function toggleDisplay() {
+		if (flagDisplayData) {
+			flagDisplayData = false;
+			document.getElementById("goalform").style.display = "block";
+			document.getElementById("clear").style.display = "inline";
+			document.getElementById("display").style.display = "inline";
+			document.getElementById("add").style.display = "none";
+			document.getElementById("goals").style.display = "none";
+
+		} else {
+			flagDisplayData = true;
+			document.getElementById("goalform").style.display = "none";
+			document.getElementById("clear").style.display = "inline";
+			document.getElementById("display").style.display = "none";
+			document.getElementById("add").style.display = "inline";
+		}
 		
+		return;
+	}
+	
+	// Grab the data from Local Storage and display it on screen
+	function displayGoalsList() {
+		
+		// toggle function to display the data
+		toggleDisplay();
+		
+		if (localStorage.length === 0) {
+			alert("There are no goals to display.");
+			
+		}
+		
+		// Create a div tag
+		var createDisplayGoalsDiv = document.createElement("div");
+		createDisplayGoalsDiv.setAttribute("id", "goals");
+		var createUList = document.createElement("ul");
+		createDisplayGoalsDiv.appendChild(createUList);
+		document.body.appendChild(createDisplayGoalsDiv);
+		document.getElementById("goals").style.display = "block";
 		for (var i = 0, j = localStorage.length; i < j; i++) {
-			currentKey = localStorage.key[i];
-			currentValue = localStorage.getItem(currentKey);
-			var myList = document.createElement("p");
-			myList.innerHTML = currentValue;
-			myDisplayDataDiv.appendChild(myList);
+			var createListItem = document.createElement("li");
+			createUList.appendChild(createListItem);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			
+			// "parse" converts string value from Local Storage back into an object
+			var goalObj = JSON.parse(value);
+		
+			var createSubUList = document.createElement("ul");
+			createListItem.appendChild(createSubUList);
+			
+			for(var n in goalObj) {
+				var createSubListItem = document.createElement("li");
+				createSubUList.appendChild(createSubListItem);
+				var goalObjText = goalObj[n][0] + " " + goalObj[n][1];
+				createSubListItem.innerHTML = goalObjText;
+			}
 		}
 	}
 	
-	var clearGoalsList = function() {
-	
-	
-	
-	
+	function clearGoalsList() {
+		if(localStorage.length === 0) {
+			alert("There are no goals to remove.");
+		} else {
+			localStorage.clear();
+			alert("All goals are deleted.");
+			window.location.reload();
+			return false;
+		}
 	}
-*/	
+	
 	// array for the goal types
 	var goalTypes = ["--Choose a Type--", "Personal", "Business", "Travel", "Finance", "Education"];
 	var goalCompleteValue = "Goal Not Yet Achieved";
 	makeGoalTypes();
+	
+	// toggle variable for checking when to hide the form to display the data or vice versa
+	var flagDisplayData = false;
 	
 	var saveGoalButton = document.getElementById("savegoal");
 	saveGoalButton.addEventListener("click", addNewGoal);
